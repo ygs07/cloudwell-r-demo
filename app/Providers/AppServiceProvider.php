09e5fd74e1387d652a6f263c3bfe50411e5bce21
Yusuf Shehu <syusuf@ubtconsults.se>
2026-03-08 16:40:42 +0100

    Auth
    made fix to limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('referrals', function (Request $request) {
-            return Limit::perMinute(1)->by(
+            return Limit::perMinute(60)->by(
                 $request->user()?->currentAccessToken()?->id ?: $request->ip()
             )
                 ->response(
