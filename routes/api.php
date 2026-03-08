b74e934bca43229c439aed4dbc91af0aa9f1e040
Yusuf Shehu <syusuf@ubtconsults.se>
2026-03-08 15:10:38 +0100

    Auth
    implemented a simple auth logic

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
